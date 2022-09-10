2793e8b6bec6c547373fca0527b0f3117f7ced4a
Piotr Krasny <krasny.piotr@gmail.com>
2022-09-10 17:03:40 +0200

    Add OwnerRecord

---
+++ records/owner.record.ts
@@ -0,0 +1,74 @@
+import {OwnerEntity} from "../types";
+import {ValidationError} from "../utils/errors";
+import {pool} from "../utils/db";
+import {FieldPacket} from "mysql2";
+
+type OwnerRecordResults = [OwnerEntity[], FieldPacket[]];
+
+export class OwnerRecord implements OwnerEntity {
+    nameandsurname: string;
+    email: string;
+    phone: string;
+
+    constructor(obj: OwnerEntity) {
+        if (!obj.nameandsurname || obj.nameandsurname.length > 30) {
+            throw new ValidationError('Musisz podać imię i nazwisko właściciela, nie mogą one łącznie przekraczać 30 znaków');
+        }
+
+        if (!obj.email || obj.email.length > 30) {
+            throw new ValidationError('Musisz podać email właściciela, nie nie może on przekraczać 30 znaków');
+        }
+
+        if (!obj.phone || obj.phone.length > 30) {
+            throw new ValidationError('Musisz podać email właściciela, nie nie może on przekraczać 30 znaków');
+        }
+
+        this.nameandsurname = obj.nameandsurname;
+        this.email = obj.email;
+        this.phone = obj.phone;
+    }
+    
+    static async getOne(nameandsurname: string): Promise<OwnerRecord | null> {
+        const [result] = await pool.execute(
+            "SELECT * FROM `owners` WHERE `nameandsurname` = :nameandsurname", {
+                nameandsurname,
+            }
+        ) as OwnerRecordResults;
+        return result.length === 0 ? null : new OwnerRecord(result[0]);
+    }
+
+    static async findAll(nameandsurname: string): Promise<OwnerEntity[]> {
+        const [results] = await pool.execute("SELECT * FROM `owner` WHERE `nameandsurname` LIKE :search", {
+            search: `%${nameandsurname}%`,
+        }) as OwnerRecordResults;
+
+        return results.map(result => {
+            const {
+                nameandsurname, email, phone
+            } = result;
+
+            return {
+                nameandsurname, email, phone
+            };
+        });
+    }
+
+    async insert(): Promise<void> {
+        await pool.execute("INSERT INTO `owners`(`nameandsurname`, `email`, `phone`) VALUES(:nameandsurname, :email, :phone)", this);
+    }
+
+    async edit(): Promise<void> {
+        await pool.execute("UPDATE `owners` SET `phone` = :phone, `email` = :email, WHERE `nameandsurname` = :nameandsurname", this
+        );
+    }
+
+    async delete(): Promise<{}> {
+        await pool.execute("DELETE FROM `owners` WHERE `nameandsurname` = :nameandsurname", {
+            nameandsurname: this.nameandsurname,
+        });
+        return {
+            isDeleted: true,
+        }
+    }
+}
+
